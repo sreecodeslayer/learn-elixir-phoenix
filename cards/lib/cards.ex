d1130c1ae3f288bab949f1f65bb80218653ff602
Sreenadh TC <kesav.tc8@gmail.com>
2019-01-01 16:30:12 +0530

    Enum module from elixir

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,10 +1,14 @@
 defmodule Cards do
 
   def hello do
-    "Hi user!"
+    "Hi user! Welcome to game of cards"
   end
 
   def create_deck do
     ["Ace","Two","Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 end
