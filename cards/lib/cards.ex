c8a01ca8dc8ee3fc6d58d02e39b85befa8e72db5
Sreenadh TC <kesav.tc8@gmail.com>
2019-01-01 16:01:12 +0530

    First module in project

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,10 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "Hi user!"
+  end
+
+  def create_deck do
+    ["Ace","Two","Three"]
   end
 end
