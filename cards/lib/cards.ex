51595fa9745246dab15e85f78b6ef7aa616d63d8
Sreenadh TC <kesav.tc8@gmail.com>
2019-01-01 16:38:20 +0530

    Enum module from elixir - membership in lists

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,14 +1,17 @@
 defmodule Cards do
-
   def hello do
     "Hi user! Welcome to game of cards"
   end
 
   def create_deck do
-    ["Ace","Two","Three"]
+    ["Ace", "Two", "Three"]
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
