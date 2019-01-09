d7c27ac3e618621db5b0e6dce0e30c915a9b985c
Sreenadh TC <kesav.tc8@gmail.com>
2019-01-09 16:45:24 +0530

    Implement the md5 hashing wrapper function for getting a md5 for the input string

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,18 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  The one stop module for making an identicon from a piece of string.
+  Create a unique image representation for every string that you may pass int.
   """
+  def main(inp) do
+    inp
+    |> get_hash
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+  Get a hash value for an input string
   """
-  def hello do
-    :world
+  def get_hash(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
   end
 end
