ff7821ccb950d3ad6ca7a7c5adf792af741a8985
Sreenadh TC <kesav.tc8@gmail.com>
2019-01-10 09:30:17 +0530

    Mirroring each row to make the identicon grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     inp
     |> get_hash
     |> pick_color
+    |> build_grid
   end
 
   @doc """
@@ -27,7 +28,24 @@ defmodule Identicon do
     # {rgblist, _junk} = Enum.split(img.hex, 3)
     # rgblist
     [r, g, b | _remain] = img.hex
-    # %Identicon.Image{hex: img.hex, rgb: [r, g, b]}
-    %Identicon.Image{img | rgb: [r, g, b]}
+    # %Identicon.Image{hex: img.hex, rgb: {r, g, b]}} # No
+    %Identicon.Image{img | rgb: {r, g, b}}
+  end
+
+  @doc """
+  Picks up the image struct, makes chunks of list for every 3 elements
+  """
+  def build_grid(img) do
+    img.hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> mirror_row
+  end
+
+  def mirror_row(current_row) do
+    # current_row = [145,46,200]
+    [first, second | _tail] = current_row
+
+    # returns as [145,46,200,46,145]
+    current_row ++ [second, first]
   end
 end
